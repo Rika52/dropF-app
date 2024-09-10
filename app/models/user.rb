e56896eaf8f4b0b7b5ba24395957b1746cd02261
Norika Niyungeko <norikaniyungeko@gmail.com>
2024-09-10 15:21:55 +0200

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :products, dependent: :destroy
+  has_many :generated_descriptions, dependent: :destroy
+  has_many :user_subscriptions, dependent: :destroy
+  has_many :subscriptions, through: :user_subscriptions
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
